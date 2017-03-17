2739c91b1390ed03d6d9ed21ad232d473c310d59
Jason Baird, Roger Landreth <roger.landreth@asynchrony.com>
2017-03-17 13:06:28 -0500

    460 Fixed a bug that prevented login
    (we weren't looking at the right file)

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const app = express();
 
 app.use('/index.html', express.static('src/index.html'));
 app.use('/login.html', express.static('src/login.html'));
-app.use('/bundle.js', express.static('src/js/bundle.js'));
-app.use('/loginBundle.js', express.static('src/js/loginBundle.js'));
+app.use('/js/bundle.js', express.static('src/js/bundle.js'));
+app.use('/js/loginBundle.js', express.static('src/js/loginBundle.js'));
 app.listen(8000, () => {});
