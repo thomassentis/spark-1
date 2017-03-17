5ade1ee5fcb82711cdeb2605c6b291751c596875
Kara Kimmel, Jason Baird <jason.baird@asynchrony.com>
2017-03-17 16:36:37 -0500

    502 Added CSS and scripts to build it

--- server.js
+++ server.js
@@ -6,4 +6,5 @@ app.use('/index.html', express.static('index.html'));
 app.use('/login.html', express.static('login.html'));
 app.use('/js/bundle.js', express.static('js/bundle.js'));
 app.use('/js/loginBundle.js', express.static('js/loginBundle.js'));
+app.use('/css/main.css', express.static('css/main.css'));
 app.listen(8000, () => {});
