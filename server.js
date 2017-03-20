76bd77bc5d477ee4974c09b0650805b1e2b2d514
Roger Landreth, Jason Baird <jason.baird@asynchrony.com>
2017-03-20 16:18:03 -0500

    485 Notifies the user when their server is started

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 
+const port = 8000;
+
 app.get('/', (req, res) => res.sendFile(__dirname + '/public/login.html'));
 app.use(express.static('public'));
-app.listen(8000, () => {});
+app.listen(port, () => {});
+
+console.log(`Server started on port ${port}...`);
