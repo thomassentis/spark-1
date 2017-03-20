8280fc52ad464a031f5c9c7e21e5117c38724ebe
Roger Landreth, Jason Baird <jason.baird@asynchrony.com>
2017-03-20 13:56:17 -0500

    460 Standardized naming convention for generated bundles & javascript files
    (loginController.js -> login.js)

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 app.get('/', (req, res) => res.sendFile(__dirname + '/login.html'));
 app.use('/index.html', express.static('index.html'));
 app.use('/login.html', express.static('login.html'));
-app.use('/js/bundle.js', express.static('js/bundle.js'));
+app.use('/js/indexBundle.js', express.static('js/indexBundle.js'));
 app.use('/js/loginBundle.js', express.static('js/loginBundle.js'));
 app.use('/css/main.css', express.static('css/main.css'));
 app.listen(8000, () => {});
