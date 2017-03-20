12f4ecc0a44b93fe305ebc37fb28a928a7bcd3f7
Jason Baird, Roger Landreth <roger.landreth@asynchrony.com>
2017-03-20 11:31:21 -0500

    468 Comment explaining the strangest part of the app so far

--- js/index.js
+++ js/index.js
@@ -3,6 +3,8 @@ const SparkService = require('./sparkService');
 
 var call = null;
 
+// When you're redirected back from Spark's login page, it grants you a temporary code which is then exchanged for an access token.
+// This process is not immediate. If you attempt to make any calls to Spark before it finishes, Spark will throw an error.
 SparkService.waitForAuthentication().then(() => {
   SparkService.register().then(() => {
     $('#submit_user_email').on('click', (event) => {
