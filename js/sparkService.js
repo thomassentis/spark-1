9c87d70a6c706be19d2d63b7fd8e1cc870c86cf2
Jason Baird <jason.baird@asynchrony.com>
2017-03-29 12:17:25 -0500

    530 Standardized export style

--- js/sparkService.js
+++ js/sparkService.js
@@ -10,92 +10,94 @@ const offerOptions = {
   offerToReceiveVideo: true
 };
 
-exports.authorize = () => {
-  return spark.authorize();
-};
+const sparkService = {
+  authorize: () => {
+    return spark.authorize();
+  },
 
-/*
-When you're redirected back from Spark's login page, it grants you a temporary
-code which is then exchanged for an access token. This process is not immediate.
-If you attempt to make any calls to Spark before it finishes, Spark will throw
-an error.
-*/
-exports.register = () => {
-  return new Promise((resolve) => {
-    let authenticationUpdate = () => {
-      if (spark.isAuthenticated) {
-        spark.off('change:isAuthenticated', authenticationUpdate);
-        spark.phone.register().then(() => {
-          resolve();
-        });
+  /*
+  When you're redirected back from Spark's login page, it grants you a temporary
+  code which is then exchanged for an access token. This process is not immediate.
+  If you attempt to make any calls to Spark before it finishes, Spark will throw
+  an error.
+  */
+  register: () => {
+    return new Promise((resolve) => {
+      let authenticationUpdate = () => {
+        if (spark.isAuthenticated) {
+          spark.off('change:isAuthenticated', authenticationUpdate);
+          spark.phone.register().then(() => {
+            resolve();
+          });
+        }
+      };
+
+      spark.on('change:isAuthenticated', authenticationUpdate);
+    });
+  },
+  listenForCall: (callback) => {
+    spark.phone.on('call:incoming', (call) => {
+      /*
+      The call:incoming event is triggered for both incoming and outgoing calls.
+      Outgoing calls are handled by SparkService.callUser(...).
+      */
+      if (call.direction === 'out') {
+        return;
       }
-    };
 
-    spark.on('change:isAuthenticated', authenticationUpdate);
-  });
-};
+      callback(call);
 
+      call.acknowledge();
+    });
+  },
 
-exports.listenForCall = (callback) => {
-  spark.phone.on('call:incoming', (call) => {
-    /*
-    The call:incoming event is triggered for both incoming and outgoing calls.
-    Outgoing calls are handled by SparkService.callUser(...).
-    */
-    if (call.direction === 'out') {
-      return;
+  answerCall: (call, options) => {
+    const constraints = Object.assign({}, defaultConstraints, options);
+    return spark.phone.createLocalMediaStream(constraints).then((localMediaStream) => {
+      return call.answer({
+        offerOptions: offerOptions,
+        constraints: constraints,
+        localMediaStream: localMediaStream
+      });
+    });
+  },
+
+  rejectCall: (call) => {
+    call.reject();
+  },
+
+  callUser: (user, options) => {
+    const constraints = Object.assign({}, defaultConstraints, options);
+    return spark.phone.createLocalMediaStream(constraints).then((localMediaStream) => {
+      return spark.phone.dial(user, {
+        offerOptions: offerOptions,
+        constraints: constraints,
+        localMediaStream: localMediaStream
+      });
+    });
+  },
+
+  hangupCall: (call) => {
+    return call.hangup();
+  },
+
+  logout: () => {
+    if(spark.isAuthenticated){
+      return spark.logout({ goto: window.location.protocol + '//' + window.location.host + '/' });
+    } else {
+      window.location = window.location.protocol + '//' + window.location.host + '/';
     }
+  },
 
-    callback(call);
-
-    call.acknowledge();
-  });
-};
-
-exports.answerCall = (call, options) => {
-  const constraints = Object.assign({}, defaultConstraints, options);
-  return spark.phone.createLocalMediaStream(constraints).then((localMediaStream) => {
-    return call.answer({
-      offerOptions: offerOptions,
-      constraints: constraints,
-      localMediaStream: localMediaStream
+  getAvatarUrl: (email) => {
+    return spark.people.list({ email: email }).then((people) => {
+      if(people.count === 0 || !people.items[0].avatar) {
+        return Promise.reject('No avatar found');
+      } else {
+        return Promise.resolve(people.items[0].avatar);
+      }
     });
-  });
-};
-
-exports.rejectCall = (call) => {
-  call.reject();
-};
-
-exports.callUser = (user, options) => {
-  const constraints = Object.assign({}, defaultConstraints, options);
-  return spark.phone.createLocalMediaStream(constraints).then((localMediaStream) => {
-    return spark.phone.dial(user, {
-      offerOptions: offerOptions,
-      constraints: constraints,
-      localMediaStream: localMediaStream
-    });
-  });
-};
-
-exports.hangupCall = (call) => {
-  return call.hangup();
-};
-
-exports.logout = () => {
-  if(spark.isAuthenticated){
-    return spark.logout({ goto: window.location.protocol + '//' + window.location.host + '/' });
-  } else {
-    window.location = window.location.protocol + '//' + window.location.host + '/';
   }
 };
 
-exports.getAvatarUrl = (email) => {
-  return spark.people.list({ email: email }).then((people) => {
-    if(people.count === 0 || !people.items[0].avatar) {
-      return Promise.reject('No avatar found');
-    } else {
-      return Promise.resolve(people.items[0].avatar);
-    }
-  });
-};
+module.exports = sparkService;
