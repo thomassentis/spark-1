8cd92c98ecc1013330d9401cd1653b477a9fb712
Chad Schwab, Kara Kimmel, Roger Landreth <roger.landreth@asynchrony.com>
2017-03-31 14:14:37 -0500

    475 Implemented toggling of receiving video
    issue with toggling video back on - video becomes lost
    
    This may be an issue on Spark's side: awaiting bug report feedback

--- js/activeCallTemplate.js
+++ js/activeCallTemplate.js
@@ -31,23 +31,47 @@ const activeCallTemplate = {
       hangupCall();
     });
 
-    let overlay = $('#outgoing-call-overlay');
-    if (currentCall.sendingVideo) {
-      overlay.hide();
-    } else {
-      $('#toggle-outgoing-video').addClass('off');
-      overlay.show();
-    }
+    initializeVideoOverlays();
 
     $('#toggle-outgoing-video').on('click', toggleSendingVideo);
     $('#toggle-outgoing-audio').on('click', toggleSendingAudio);
+
+    $('#toggle-incoming-video').on('click', toggleReceivingVideo);
 
     $('#logout-button').on('click', hangupCall);
   }
 };
 
+function initializeVideoOverlays() {
+  let outgoingOverlay = $('#outgoing-call-video-overlay');
+  if (currentCall.sendingVideo) {
+    outgoingOverlay.hide();
+  } else {
+    $('#toggle-outgoing-video').addClass('off');
+    outgoingOverlay.show();
+  }
+
+  let incomingOverlay = $('#incoming-call-video-overlay');
+  if (currentCall.receivingVideo) {
+    incomingOverlay.hide();
+  } else {
+    $('#toggle-incoming-video').addClass('off');
+    incomingOverlay.show();
+  }
+}
+
+function toggleReceivingVideo() {
+  let overlay = $('#incoming-call-video-overlay');
+  currentCall.receivingVideo ? overlay.show() : overlay.hide();
+  $('#toggle-incoming-video').toggleClass('off');
+
+  currentCall.toggleReceivingVideo().then(() => {
+    updateRemoteStream();
+  });
+}
+
 function toggleSendingVideo() {
-  let overlay = $('#outgoing-call-overlay');
+  let overlay = $('#outgoing-call-video-overlay');
   currentCall.sendingVideo ? overlay.show() : overlay.hide();
   $('#toggle-outgoing-video').toggleClass('off');
 
