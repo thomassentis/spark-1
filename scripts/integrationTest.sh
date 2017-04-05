b13c0a755648616e7996bf7a727bda25464b1709
phil.king <phil.king@asynchrony.com>
2017-04-05 14:29:49 -0500

    550 Improved output of integration tests, and added retry loop

--- scripts/integrationTest.sh
+++ scripts/integrationTest.sh
@@ -1,15 +1,20 @@
 #!/usr/bin/env bash
 
 function test() {
-  webdriver-manager update
-  nohup npm start &
-  nohup webdriver-manager start &
+
+  echo "Updating webdriver-manager and node packages"
+  nohup webdriver-manager update &>/dev/null
+  nohup npm update &>/dev/null
+
+  echo "Spinning up local servers"
+  nohup npm start &>/dev/null &
+  nohup webdriver-manager start &>/dev/null &
 
   while ! nc -z localhost 8000; do
     sleep 0.1
   done
 
-  protractor spec/support/conf.js
+  protractorResult=$(protractor spec/support/conf.js)
 
   nodeServerPID=$(ps S | tail -10 | cut -c1-5 | head -n 1)
   webdriverPID1=$(ps S | tail -11 | cut -c1-5 | head -n 1)
@@ -21,12 +26,27 @@ function test() {
   kill $webdriverPID3
   kill $chromePID1
 
-  rm nohup.out
-  rm npm-debug.log
+  echo $protractorResult | grep -q "0 failures"
+  if [ $? -eq 0 ]; then
+    result=0
+  else
+    echo "Failed on attempt "$n" of 3"
+  fi
 
 }
 
+n=0
+result=1
+until [ "$n" -gt 2 -o "$result" -eq 0 ]
+do
+  n=$[$n+1]
+  test
+done
 
-test
-
-# until test && $protractorResult; do :; done
+if [ "$result" -eq 0 ]; then
+  echo "Tests passed after "$n" attempt(s)"
+  exit 0
+else
+  echo "Tests still failing after "$n" attempt(s)"
+  exit 1
+fi
